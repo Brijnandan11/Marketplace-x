0078b23c309588148a1eee14109f948c27585f9d
Brijnandan11 <brij19069@gmail.com>
2026-06-21 11:24:09 +0530

    Add environment congfiguration

---
+++ src/config/env.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+module.exports = {
+    port: process.env.PORT,
+
+    dbHost: process.env.DB_HOST,
+    dbPort: process.env.DB_PORT,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+
+    nodeEnv: process.env.NODE_ENV
+};
