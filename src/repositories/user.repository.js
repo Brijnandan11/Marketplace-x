7cd7625702b8f62b7cdfda22133c3bfa56a89809
Brijnandan11 <brij19069@gmail.com>
2026-06-21 15:57:59 +0530

    create user repository

---
+++ src/repositories/user.repository.js
@@ -0,0 +1,12 @@
+const pool = require("../db")
+
+async function getAllUsers(){
+    const result = await pool.query(
+        `SELECT * FROM users`
+    )
+    return result.rows;
+}
+
+module.exports = {
+    getAllUsers
+}
