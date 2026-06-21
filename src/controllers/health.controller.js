ad20b06ea28d3129b0fb11906d3c259cf4e6ddae
Brijnandan11 <brij19069@gmail.com>
2026-06-21 10:47:41 +0530

    Reinitializing the whole project from start

--- src/controllers/health.controller.js
+++
@@ -1,6 +0,0 @@
-exports.healthCheck = async(req,res)=>{
-    return res.status(200).json({
-        success: true,
-        message: "Marketplacex API Running"
-    })
-}
--- src/middlewares/requestLogger.middleware.js
+++
@@ -1,7 +0,0 @@
-const pinoHttp = require("pino-http")
-
-const logger = require("../config/logger")
-
-module.exports = pinoHttp({
-    logger
-})
