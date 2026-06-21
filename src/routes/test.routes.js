b4c0b80ef53cd767847a3ebce3e4862c81713180
Brijnandan11 <brij19069@gmail.com>
2026-06-21 16:27:12 +0530

    install tools to improve code quality

--- src/routes/test.routes.js
+++ src/routes/test.routes.js
@@ -1,7 +1,7 @@
 const userRepository = require("../repositories/user.repository")
 
 app.get("/users-test",async(req,res)=>{
-    const users = await userRepository.getAllUsers();
+    const users = await  userRepository.getAllUsers();
 
     return res.json(users);
 })
