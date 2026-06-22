576aad2872a448ec15550e96693d895a87bc04af
Brijnandan11 <brij19069@gmail.com>
2026-06-22 20:01:52 +0530

    add service layer in register

---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcryptjs");
+
+const authRepository = require("./auth.repository");
+
+const logger = require("../../config/logger");
+
+async function registerUser(data) {
+  logger.info(
+    {
+      email: data.email,
+    },
+    "User registration start",
+  );
+
+  const existingUser = await authRepository.findUserByEmail(data.email);
+
+  if (existingUser) {
+    logger.warn(
+      {
+        email: data.email,
+      },
+      "Registration failed: email already exist",
+    );
+
+    throw new Error("Email already exist");
+  }
+
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+
+  const user = await authRepository.createUser({
+    name: data.name,
+    email: data.email,
+    password: hashedPassword,
+    role: "CUSTOMER",
+  });
+  return user;
+}
+
+module.exports = {
+  registerUser,
+};
