63e45d5a48c2827672dce812be1bf9cecf57edaf
Brijnandan11 <brij19069@gmail.com>
2026-06-22 19:41:40 +0530

    add repository code for github

---
+++ src/modules/auth/auth.repository.js
@@ -0,0 +1,19 @@
+const pool = require("../../db");
+
+async function findUserByEmail(email) {
+  const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [email]);
+  return result.rows[0];
+}
+
+async function createUser(user) {
+  const result = await pool.query(
+    `INSERT INTO users(name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING *`,
+    [user.name, user.email, user.password, user.role],
+  );
+  return result.rows[0];
+}
+
+module.exports = {
+  findUserByEmail,
+  createUser,
+};
