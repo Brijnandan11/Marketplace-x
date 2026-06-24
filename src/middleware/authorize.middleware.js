7d6e55d0d9e387a48bec3ced8e5f014d1f6773da
Brijnandan11 <brij19069@gmail.com>
2026-06-24 17:01:40 +0530

    add authorize middleware

---
+++ src/middleware/authorize.middleware.js
@@ -0,0 +1,48 @@
+const logger = require("../config/logger");
+
+function authorize(...roles) {
+  return (req, res, next) => {
+    try {
+      const userRole = req.user.roles;
+
+      if (!roles.includes(userRole)) {
+        logger.warn(
+          {
+            userId: req.user.userId,
+            role: userRole,
+          },
+          "Authentication failed",
+        );
+
+        return res.status(403).json({
+          success: false,
+          message: "Forbidden",
+        });
+      }
+
+      logger.info(
+        {
+          userId: req.user.userId,
+          role: userRole,
+        },
+        "Authorization successfull",
+      );
+
+      next();
+    } catch (error) {
+      logger.error(
+        {
+          error: error.message,
+        },
+        "Authorization error",
+      );
+
+      return res.status(500).json({
+        success: false,
+        message: "Authorization failed",
+      });
+    }
+  };
+}
+
+module.exports = authorize;
