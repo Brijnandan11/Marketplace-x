aca1a2c3b808f15b4b56d5f8645b2ab0411c8dea
Brijnandan11 <brij19069@gmail.com>
2026-06-26 17:20:03 +0530

    add create vendor repository

--- src/modules/vendor/vendor.repository.js
+++ src/modules/vendor/vendor.repository.js
@@ -7,6 +7,35 @@ async function findVendorBySlug(slug) {
   return result.rows[0];
 }
 
+async function createVendor(data) {
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    const vendorResult = await client.query(
+      `INSERT INTO vendors (name, slug, description) VALUES ($1, $2, $3) RETURNING * `,
+      [data.name, data.slug, data.description],
+    );
+    const vendor = vendorResult.rows[0];
+
+    await client.query(
+      `INSERT INTO vendor_users (vendor_id, user_id) VALUES ($1, $2) `,
+      [vendor.id, data.id],
+    );
+
+    await client.query("COMMIT");
+
+    return vendor;
+  } catch (error) {
+    await client.query("ROOLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
 module.exports = {
   findVendorBySlug,
+  createVendor,
 };
