1f010d12eb81d046ee992d739a19f8fca215b940
Brijnandan11 <brij19069@gmail.com>
2026-06-27 18:24:32 +0530

    add updatevendorstatus service

--- src/modules/vendor/vendor.service.js
+++ src/modules/vendor/vendor.service.js
@@ -30,6 +30,43 @@ async function createVendor(data) {
   return vendor;
 }
 
+async function updateVendorStatus(data) {
+  logger.info({
+    vendorId: data.vendorId,
+    status: data.status,
+    adminId: data.adminId,
+  });
+  const vendor = await vendorRepository.findVendorById(data.vendorId);
+
+  if (!vendor) {
+    logger.warn(
+      {
+        vendorId: data.vendorId,
+      },
+      "Vendor does not found",
+    );
+
+    throw new Error("Vendor did not found");
+  }
+
+  const updatedVendorStatus = await vendorRepository.updateVendorStatus(
+    data.vendorId,
+    data.status,
+  );
+
+  logger.info(
+    {
+      vendorId: data.vendorId,
+      status: data.status,
+      adminId: data.adminId,
+    },
+    "Vendor status updated successfully",
+  );
+
+  return updatedVendorStatus;
+}
+
 module.exports = {
   createVendor,
+  updateVendorStatus,
 };
