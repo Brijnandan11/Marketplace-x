50776366dd017ae3f26acb026216ab75a0b4263b
Brijnandan11 <brij19069@gmail.com>
2026-06-27 23:00:39 +0530

    add routes

--- src/modules/vendor/vendor.routes.js
+++ src/modules/vendor/vendor.routes.js
@@ -6,6 +6,15 @@ const vendorController = require("./vendor.controller");
 
 const authenticate = require("../../middleware/auth.middleware");
 
+const authorize = require("../../middleware/auth.middleware");
+
 router.post("/", authenticate, vendorController.createVendor);
+
+router.post(
+  "/:id/status",
+  authenticate,
+  authorize("ADMIN"),
+  vendorController.updateVendorStatus,
+);
 
 module.exports = router;
